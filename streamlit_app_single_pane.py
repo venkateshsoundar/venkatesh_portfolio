61992dbdf4ce54dfd6d7196edc2571429dfc3817
Venkatesh Soundararajan <114813214+venkateshsoundar@users.noreply.github.com>
2025-05-28 01:00:20 -0600

    new

--- streamlit_app_single_pane.py
+++ streamlit_app_single_pane.py
@@ -450,6 +450,12 @@ st.markdown("""
         background: #ffd166;
         color: #22304A !important;
     }
+    .section-pane {
+  margin-bottom: 36px;
+}
+.card {
+  margin-bottom: 0;
+}
     </style>
 """, unsafe_allow_html=True)
 
@@ -469,6 +475,12 @@ st.markdown("""
 }
 .navbar a:hover {
     background: #ffd16633; color: #fff;
+}
+.section-pane {
+  margin-bottom: 36px;
+}
+.card {
+  margin-bottom: 0;
 }
 </style>
 <div class="navbar">
@@ -558,6 +570,12 @@ st.markdown("""
 .profile-right {
     flex: 1;
 }
+.section-pane {
+  margin-bottom: 36px;
+}
+.card {
+  margin-bottom: 0;
+}
 </style>
 """, unsafe_allow_html=True)
 
@@ -638,6 +656,12 @@ st.markdown(
         font-weight: 700;
         font-size: 1.8rem;
       }}
+      .section-pane {
+  margin-bottom: 36px;
+}
+.card {
+  margin-bottom: 0;
+}
     </style>
     """,
     unsafe_allow_html=True,
@@ -821,6 +845,12 @@ st.markdown("""
 @media (max-width: 900px) {
   .hero-card {flex-direction: column;align-items: center;}
   .hero-right, .hero-left {max-width:100%;padding:28px 8vw 12px;}
+}
+.section-pane {
+  margin-bottom: 36px;
+}
+.card {
+  margin-bottom: 0;
 }
 </style>
 

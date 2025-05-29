9ac281c447d0758406403b53e6f92d4312f3ad80
Venkatesh Soundararajan <114813214+venkateshsoundar@users.noreply.github.com>
2025-05-29 17:16:19 -0600

    Update streamlit_app_single_pane.py

--- streamlit_app_single_pane.py
+++ streamlit_app_single_pane.py
@@ -487,7 +487,7 @@ html, body, .stApp {
 <!-- Add padding to prevent overlapping -->
 <div class="main-content"></div>
 """, unsafe_allow_html=True)
-
+st.markdown("""<div style="margin-top: 90px;"></div>""", unsafe_allow_html=True)
 
 # --- Custom Styling ---
 st.markdown("""

dc6a6391addfb460d7197d6162a2440bb0ac5f44
Venkatesh Soundararajan <114813214+venkateshsoundar@users.noreply.github.com>
2025-05-30 18:15:25 -0600

    Update streamlit_app_single_pane.py

--- streamlit_app_single_pane.py
+++ streamlit_app_single_pane.py
@@ -6,7 +6,7 @@ import openai
 import pandas as pd
 
 # ---- PAGE CONFIG & GLOBAL CSS ----
-st.set_page_config(page_title="Venkatesh Portfolio", layout="wide")
+st.set_page_config(page_title="Venkatesh Portfolio", layout="mobile")
 
 # ---- FREEZED (FIXED) NAVIGATION BAR ----
 st.markdown("""

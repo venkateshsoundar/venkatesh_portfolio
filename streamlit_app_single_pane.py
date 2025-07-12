ee33760b549011bad30acf099e328bb1aa2490f7
Venkatesh Soundararajan <114813214+venkateshsoundar@users.noreply.github.com>
2025-07-12 11:55:56 -0600

    Update streamlit_app_single_pane.py

--- streamlit_app_single_pane.py
+++ streamlit_app_single_pane.py
@@ -8,6 +8,16 @@ import pandas as pd
 # ---- PAGE CONFIG & GLOBAL CSS ----
 st.set_page_config(page_title="Venkatesh Portfolio", layout="wide")
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9TDXL1JB47"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9TDXL1JB47');
+</script>
+
 # ---- FREEZED (FIXED) NAVIGATION BAR ----
 st.markdown("""
 <style>

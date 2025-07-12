5fcadd99f39101a35c6b3654223df51e06ba0f6f
Venkatesh Soundararajan <114813214+venkateshsoundar@users.noreply.github.com>
2025-07-11 22:44:01 -0600

    Update streamlit_app_single_pane.py

--- streamlit_app_single_pane.py
+++ streamlit_app_single_pane.py
@@ -44,7 +44,6 @@ body {
     background: #1F2A44;
     padding: 12px 0 10px 0;
     border-radius: 0 0 18px 18px;
-    margin-bottom: 10px;
     position: sticky;
     top: 0;
     z-index: 100;
@@ -78,7 +77,6 @@ body {
     background: #1F2A44;
     padding: 12px 0 10px 0;
     border-radius: 0 0 18px 18px;
-    margin-bottom: 10px;
     position: sticky;
     top: 0;
     z-index: 100;

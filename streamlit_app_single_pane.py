0eef232960573957a3e609892fd30575634c3c33
Venkatesh Soundararajan <114813214+venkateshsoundar@users.noreply.github.com>
2025-07-11 23:40:21 -0600

    Update streamlit_app_single_pane.py

--- streamlit_app_single_pane.py
+++ streamlit_app_single_pane.py
@@ -34,7 +34,7 @@ body {
     display: flex;
     flex-direction: column;
     justify-content: flex-end;
-    padding-bottom: -50px;
+    padding-bottom: -60px;
     }
 
 /* Flex styling for links */

61e59ea2c5d684716138225aad3cc9096145bd2a
Venkatesh Soundararajan <114813214+venkateshsoundar@users.noreply.github.com>
2025-07-15 19:10:53 -0600

    Update streamlit_app_single_pane.py

--- streamlit_app_single_pane.py
+++ streamlit_app_single_pane.py
@@ -9,6 +9,20 @@ import pandas as pd
 st.set_page_config(page_title="Venkatesh Portfolio", layout="wide")
 
 
+import streamlit.components.v1 as components
+
+# Google Analytics script using components.html
+components.html("""
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9TDXL1JB47"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-9TDXL1JB47');
+</script>
+""", height=0)
+
 
 st.markdown("""
 <!-- Google tag (gtag.js) -->

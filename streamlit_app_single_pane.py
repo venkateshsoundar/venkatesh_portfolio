77f35cfc15a9995b879341fe1a2f75c27c1ee775
Venkatesh Soundararajan <114813214+venkateshsoundar@users.noreply.github.com>
2025-08-15 02:07:43 -0600

    Update streamlit_app_single_pane.py

--- streamlit_app_single_pane.py
+++ streamlit_app_single_pane.py
@@ -735,7 +735,7 @@ st.markdown(
       .welcome-card {{
         background: url("{gif_url}") center/cover no-repeat;
         border-radius: 16px;
-        padding: 1rem;
+        padding: 3rem;
         color: white;
         min-height: 180px;
         display: flex;
@@ -743,6 +743,7 @@ st.markdown(
         justify-content: center;
         text-align: center;
         margin-bottom:24px;
+        margin-top:24px;
         box-shadow: 0 6px 30px 0 rgba(60,100,180,0.11), 0 1.5px 8px 0 rgba(60,60,90,0.08);
         transition: transform .35s cubic-bezier(.33,1.6,.66,1), box-shadow .33s;
         position: relative;
